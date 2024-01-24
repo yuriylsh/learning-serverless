d81d7078db27908af12dbff1977a3ca5a615813b
Yuriy Lyeshchenko <ylyeshchenko@aais.com>
2024-01-24 14:54:04 -0600

    implementing PUT to DynamoDb table

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,17 +1,55 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {v4 as uuid} from 'uuid';
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+
+const dynamodb = new DynamoDBClient({
+  region: 'us-east-2'
+});
 
 export const hello = async (
-  event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent,
+  context: Context
 ): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        event,
+        context
       },
       null,
       2
     ),
   };
 };
+
+export const createAuction = async(event: APIGatewayProxyEvent, context: Context
+): Promise<APIGatewayProxyResult> => {
+  try{
+    const jsonBody = JSON.parse(event.body!);
+    const {title} = jsonBody as {title: string};
+    const now = new Date();
+    const auction = {
+      id: uuid(),
+      title,
+      status: 'OPEN',
+      createdAt: now.toISOString()
+    };
+    await dynamodb.send(new PutItemCommand({
+      TableName: 'AuctionsTable',
+      Item: {
+        id: {S: uuid()},
+        title: {S: title},
+        status: {S: 'OPEN'},
+        cretedAt: {S: now.toISOString()}
+      }
+    }));
+    return {
+      statusCode: 201,
+      body: JSON.stringify(auction),
+    };
+  }catch(e){
+    console.log("Error:", e);
+    throw e;
+  }
+}
